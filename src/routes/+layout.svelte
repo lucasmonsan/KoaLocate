2784a0a16e9ccec1732f1b225c6b3668a43b974f
lucasmonsan <lucasmonsan@gmail.com>
2025-12-10 18:58:44 -0300

    Favicon, css reset, fonts, variables and colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import Map from '$lib/components/map/Map.svelte';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
@@ -8,4 +10,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Map />
 {@render children()}
